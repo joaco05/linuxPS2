13bab31f1058eaa66741f0d91d8a3f273702a24d
Fredrik Noring <noring@nocrew.org>
2019-05-08 17:19:48 +0200

    MIPS: PS2: FB: Disable GIF DMA completion interrupts
    The GIF asserts DMA completion interrupts that are uninteresting
    because DMA operations are fast enough to busy-wait for.
    
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

--- drivers/video/fbdev/ps2fb.c
+++ drivers/video/fbdev/ps2fb.c
@@ -2275,6 +2275,12 @@ static int ps2fb_probe(struct platform_device *pdev)
 	}
 	fb_set_cmap(&info->cmap, info);
 
+	/*
+	 * The GIF asserts DMA completion interrupts that are uninteresting
+	 * because DMA operations are fast enough to busy-wait for.
+	 */
+	disable_irq(IRQ_DMAC_GIF);
+
 	if (register_framebuffer(info) < 0) {
 		fb_err(info, "register_framebuffer failed\n");
 		err = -EINVAL;
@@ -2286,6 +2292,7 @@ static int ps2fb_probe(struct platform_device *pdev)
 	return 0;
 
 err_register_framebuffer:
+	enable_irq(IRQ_DMAC_GIF);
 	fb_dealloc_cmap(&info->cmap);
 err_alloc_cmap:
 err_find_mode:
@@ -2314,6 +2321,7 @@ static int ps2fb_remove(struct platform_device *pdev)
 		fb_err(info, "Failed to complete GIF DMA transfer\n");
 		err = -EBUSY;
 	}
+	enable_irq(IRQ_DMAC_GIF);
 	free_page((unsigned long)par->package.buffer);
 
 	return err;
